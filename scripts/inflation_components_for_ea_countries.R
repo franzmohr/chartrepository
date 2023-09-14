1375abe7cff8b17a0c6e9e096ad3929b2a765403
Franz Mohr <franz.x.mohr@outlook.com>
2023-09-14 17:03:07 +0200

    Inflation of euro area countries

--- scripts/inflation_components_for_ea_countries.R
+++ scripts/inflation_components_for_ea_countries.R
@@ -8,9 +8,34 @@ library(ggplot2)
 library(tidyr)
 library(zoo)
 
+ctry_code <- "AT"
+
+lang <- "en"
+
+if (lang == "de") {
+  temp_var_levels <- c("SERV", "IGD_NNRG", "NRG", "FOOD")
+  temp_var_labels <- c("Dienstleistungen", "Industriegüter ohne Energie",
+                       "Energie", "Nahrungsmittel")
+  temp_line_levels <- c("CP00", "TOT_X_NRG_FOOD")
+  temp_line_labels <- c("HVPI-Inflation", "Kerninflation (ohne Energie, Nahrungsmittel)")
+  temp_title <- paste0("Beitrag zur Inflation (", ctry_code , ")")
+  temp_subtitle <- "Inflationsraten in %; Beiträge der Komponenten in Prozentpunkten"
+  temp_caption <- "Quelle: Eurostat. Idee: OeNB (2020). Gesamtwirtschaftliche Prognose der OeNB für Österreich 2020 bis 2023."
+}
+if (lang == "en") {
+  temp_var_levels <- c("SERV", "IGD_NNRG", "NRG", "FOOD")
+  temp_var_labels <- c("Services", "Non-energy industrial goods",
+                       "Energy", "Food including alcohol and tobacco")
+  temp_line_levels <- c("CP00", "TOT_X_NRG_FOOD")
+  temp_line_labels <- c("HCPI-inflation", "Core inflation (w/o energy, food)")
+  temp_title <- paste0("Contribution to inflation (", ctry_code, ")")
+  temp_subtitle <- "Inflation in %; Contribution of component in percentage points"
+  temp_caption <- "Source: Eurostat. Idea: OeNB (2020). Gesamtwirtschaftliche Prognose der OeNB für Österreich 2020 bis 2023."
+}
+
 # Weights
 weights <- get_eurostat(id = "prc_hicp_inw",
-                        filters = list(geo = "AT",
+                        filters = list(geo = ctry_code,
                                        coicop = c("FOOD", "NRG", "IGD_NNRG", "SERV")))
 
 weights <- weights %>%
@@ -20,7 +45,7 @@ weights <- weights %>%
 
 # Growth prc_hicp_manr
 index <- get_eurostat(id = "prc_hicp_manr",
-                      filters = list(geo = "AT",
+                      filters = list(geo = ctry_code,
                                      coicop = c("CP00", "TOT_X_NRG_FOOD", "FOOD", "NRG", "IGD_NNRG", "SERV"))) %>%
   mutate(time = as.Date(paste0(as.character(time), "-01"))) %>%
   filter(time >= "2019-01-01",
@@ -35,31 +60,25 @@ comp <- index %>%
   mutate(weight = weight / sum(weight),
          values = values * weight) %>%
   ungroup() %>%
-  mutate(var_de = factor(coicop, levels = c("SERV", "IGD_NNRG", "NRG", "FOOD"),
-                         labels = c("Dienstleistungen", "Industriegüter ohne Energie",
-                                    "Energie", "Nahrungsmittel")),
-         var_en = factor(coicop, levels = c("SERV", "IGD_NNRG", "NRG", "FOOD"),
-                         labels = c("Services", "Non-energy industrial goods",
-                                    "Energy", "Food including alcohol and tobacco")))
+  mutate(var = factor(coicop, levels = temp_var_levels,
+                      labels = temp_var_labels))
 
 line <- index %>%
   filter(coicop %in% c("CP00", "TOT_X_NRG_FOOD"),
          !is.na(values)) %>%
-  mutate(line_de = factor(coicop, levels = c("CP00", "TOT_X_NRG_FOOD"),
-                          labels = c("HVPI-Inflation", "Kerninflation (ohne Energie, Nahrungsmittel)")),
-         line_en = factor(coicop, levels = c("CP00", "TOT_X_NRG_FOOD"),
-                          labels = c("HCPI-inflation", "Core inflation (w/o energy, food)")))
+  mutate(line = factor(coicop, levels = temp_line_levels,
+                          labels = temp_line_labels))
 
 g <- ggplot(comp, aes(x = time, y = values)) +
-  geom_col(aes(fill = var_de), alpha = 1) +
-  geom_line(data = line, aes(linetype = line_de), linewidth = 1.2) +
+  geom_col(aes(fill = var), alpha = 1) +
+  geom_line(data = line, aes(linetype = line), linewidth = 1.2) +
   scale_x_date(expand = c(.01, 0)) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   guides(fill = guide_legend(ncol = 2),
          linetype = guide_legend(ncol = 2, keywidth = 1.5)) +
-  labs(title = "Beitrag zur Inflation (Österreich)", 
-       subtitle = "Inflationsraten in %; Beiträge der Komponenten in Prozentpunkten",
-       caption = "Quelle: Eurostat. Idee: OeNB (2020). Gesamtwirtschaftliche Prognose der OeNB für Österreich 2020 bis 2023.") +
+  labs(title = temp_title, 
+       subtitle = temp_subtitle,
+       caption = temp_caption) +
   theme(legend.position="bottom",
         legend.box = "vertical",
         legend.title = element_blank(),
